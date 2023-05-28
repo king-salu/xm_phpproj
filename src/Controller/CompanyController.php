3e903f832a926b2de1f61da8408039e743728839
king-salu <johndele94@gmail.com>
2023-05-28 03:31:17 +0100

    clean up

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -12,11 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\Bridge\Sendgrid\Transport;
-use Symfony\Component\Mailer\Bridge\Sendgrid\Transport\SendgridApiTransport;
-use Symfony\Component\Mailer\Mailer;
-use Symfony\Component\Mailer\Transport as MailerTransport;
-use Symfony\Component\Mailer\Transport\SendmailTransport;
 
 class CompanyController extends AbstractController
 {
